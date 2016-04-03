cc4df0c834c9329d34b46fc0928a5bc4c4901b9f
yuriidanylchenko <yurii.danylchenko@gmail.com>
2016-04-03 23:22:38 +0300

    add: регистрация пользователя

--- course task/course task/WindowReg.xaml.cs
+++ course task/course task/WindowReg.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Npgsql;
 
 namespace course_task
 {
@@ -22,6 +23,32 @@ namespace course_task
         public WindowReg()
         {
             InitializeComponent();
+        }
+
+        private void buttonReg_Click(object sender, RoutedEventArgs e)
+        {
+            // try
+            //{
+           // bool insertdata = false;
+                
+                NpgsqlConnection conn = new NpgsqlConnection("Server=192.168.0.101; Port=5432; User Id=yurii; Password=yurii2104; Database=database");
+                conn.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand("insert into database values (@id,@mail,@login,@pass)", conn);
+                cmd.Parameters.Add(new NpgsqlParameter("@id", int.Parse(textBoxId.Text)));
+                cmd.Parameters.Add(new NpgsqlParameter("@mail", textBoxMail.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("@login", textBoxLog.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("@pass", textBoxPass.Text));
+            int count = cmd.ExecuteNonQuery();
+            if (count == 1)
+            { MessageBox.Show("ok"); }
+            //    cmd.ExecuteNonQuery();
+               conn.Close();
+            //}
+            //catch (Exception msg)
+            //{
+            //    MessageBox.Show(msg.ToString());
+            //    throw;
+            //}
         }
     }
 }
